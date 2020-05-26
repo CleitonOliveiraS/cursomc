f7aa0e813eebd47c4a712ef957e18fadda7f0542
cleiton <36275279+CleitonOliveiraS@users.noreply.github.com>
2020-05-26 01:57:51 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/cleiton/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.cleiton.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cleiton.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
